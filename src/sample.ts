4506862f7db47483335574810b56e978af2be0fd
Andrew Peterson <andy@ndpsoftware.com>
2021-05-22 10:19:00 -0700

    Use full option interface in sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -1,16 +1,15 @@
 import { cajoler } from './cajoler'
-cajoler(
-  'https',
-  '<h2>Switch to a secure connection?</h2><p>Sites without the "s" in "https:" are susceptible to "wire-tapping" attacks. Curious or mallicious observers can easily see what you are doing. Although Amp-what has no personal information, it does support Https. </p>',
-  {
-    showFilter: previousButton =>
-      window.location.protocol === 'http:' && previousButton !== 'no',
-    yes: {
-      verb: 'Use a Secure Connection',
-      callback: () => (window.location.protocol = 'https:')
-    },
-    no: {
-      verb: 'Dismiss'
-    }
+
+cajoler({
+  key: 'https',
+  nudgePrompt: '<h2>Switch to a secure connection?</h2>',
+  showFilter: previousButton =>
+    window.location.protocol === 'http:' && previousButton !== 'no',
+  yes: {
+    verb: 'Use a Secure Connection',
+    callback: () => (window.location.protocol = 'https:')
+  },
+  no: {
+    verb: 'Dismiss'
   }
-)
+})
